5582bf2ea70ea90011265e4179f30e6e7600fd3d
abouabra <aymanbouabra@gmail.com>
2023-01-15 18:59:06 +0100

    1337

--- make_philos.c
+++ make_philos.c
@@ -1,4 +1,3 @@
-
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
@@ -6,12 +5,31 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: abouabra < abouabra@student.1337.ma >      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/07 12:22:31 by abouabra          #+#    #+#             */
-/*   Updated: 2023/01/07 16:10:44 by abouabra         ###   ########.fr       */
+/*   Created: 2023/01/15 18:44:51 by abouabra          #+#    #+#             */
+/*   Updated: 2023/01/15 18:58:14 by abouabra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+void	philo_food(t_args *vars, int i)
+{
+	pthread_mutex_lock(&vars->mutex[i]);
+	print_status(vars, IS_TAKING_FORK, i + 1);
+	if (vars->n_of_philos > 1)
+	{
+		pthread_mutex_lock(&vars->mutex[(i + 1) % vars->n_of_philos]);
+		print_status(vars, IS_TAKING_FORK, i + 1);
+	}
+	print_status(vars, IS_EATING, i + 1);
+	ft_usleep(vars, vars->time_to_eat);
+	if (vars->n_of_philos > 1)
+		vars->eating_duration[i] = get_time();
+	vars->eating_times[i]++;
+	pthread_mutex_unlock(&vars->mutex[i]);
+	if (vars->n_of_philos > 1)
+		pthread_mutex_unlock(&vars->mutex[(i + 1) % vars->n_of_philos]);
+}
 
 void	*handle_philo(void *arg)
 {
@@ -20,41 +38,21 @@ void	*handle_philo(void *arg)
 
 	vars = (t_args *)arg;
 	i = *(vars->index);
-	if(i % 2 == 1)
+	if (i % 2 == 1)
 		usleep(100);
-	
 	while (1)
 	{
-		if(vars->kill_yourself)
-			return 0;
-
-		pthread_mutex_lock(&vars->mutex[i]);
-		print_status(vars, IS_TAKING_FORK, i + 1);
-
-		if(vars->n_of_philos > 1)
-		{
-			pthread_mutex_lock(&vars->mutex[(i + 1) % vars->n_of_philos]);
-			print_status(vars, IS_TAKING_FORK, i + 1);
-		}
-		print_status(vars, IS_EATING, i + 1);
-		ft_usleep(vars, vars->time_to_eat);
-		if(vars->n_of_philos > 1)
-			vars->eating_duration[i] = get_time();
-		vars->eating_times[i]++;
-
-		pthread_mutex_unlock(&vars->mutex[i]);
-		if(vars->n_of_philos > 1)
-			pthread_mutex_unlock(&vars->mutex[(i + 1) % vars->n_of_philos]);
+		if (vars->kill_yourself)
+			return (0);
+		philo_food(vars, i);
 		print_status(vars, IS_SLEEPING, i + 1);
-
 		ft_usleep(vars, vars->time_to_sleep);
 		print_status(vars, IS_THINKING, i + 1);
-
 	}
 	return (NULL);
 }
 
-void	make_philos(t_args *vars)
+void	init_values(t_args *vars)
 {
 	int	i;
 
@@ -64,8 +62,8 @@ void	make_philos(t_args *vars)
 	memset(vars->eating_times, 0, sizeof(vars->eating_times));
 	memset(vars->eating_duration, 0, sizeof(vars->eating_duration));
 	i = -1;
-		while (++i < vars->n_of_philos)
-			vars->eating_duration[i] = get_time();
+	while (++i < vars->n_of_philos)
+		vars->eating_duration[i] = get_time();
 	i = -1;
 	while (++i < vars->n_of_philos)
 		pthread_mutex_init(&(vars->mutex[i]), NULL);
@@ -77,27 +75,27 @@ void	make_philos(t_args *vars)
 			return ;
 		usleep(100);
 	}
+}
 
-	while(1)
+void	make_philos(t_args *vars)
+{
+	int	i;
+
+	init_values(vars);
+	while (1)
 	{
 		i = -1;
-		while(++i < vars->n_of_philos)
+		while (++i < vars->n_of_philos)
 		{
-			if (get_interval(vars->eating_duration[i],get_time()) >= vars->time_to_die)
+			if (get_interval(vars->eating_duration[i],
+					get_time()) >= vars->time_to_die
+				|| (vars->n_times_must_eat && is_every_one_ate(vars)))
 			{
-				print_status(vars, IS_DEAD, i + 1);
-
+				if (vars->n_times_must_eat && is_every_one_ate(vars))
+					print_status(vars, IS_FULL, i + 1);
+				else
+					print_status(vars, IS_DEAD, i + 1);
 				vars->kill_yourself = 1;
-
-				end_phase(vars);
-				exit(0);
-			}
-			if (vars->n_times_must_eat && is_every_one_ate(vars))
-			{
-				print_status(vars, IS_FULL, i + 1);
-				
-				vars->kill_yourself = 1;
-
 				end_phase(vars);
 				exit(0);
 			}
